67d2c4cdf05fe1722bd8cb32a571ca1d144c35b1
psk <psk3060@naver.com>
2019-06-21 17:21:53 +0900

    Counter 파일 분할

--- 7/src/App.vue
+++ 7/src/App.vue
@@ -1,29 +1,17 @@
 <template>
   <div>
-    <p>{{count}}</p>
-    <p>
-      <button type="button" name="button" @click="increment">+</button>
-      <button type="button" name="button" @click="decrement">-</button>
-    </p>
+    <Counter></Counter>
   </div>
 
 </template>
 
 <script>
 
+import Counter from './components/Counter.vue'
+
 export default {
-  computed : {
-    count : function() {
-      return this.$store.state.count;
-    }
-  },
-  methods: {
-    increment : function() {
-      this.$store.commit('increment');
-    },
-    decrement : function() {
-      this.$store.commit('decrement');
-    }
+  components : {
+    'Counter' : Counter
   }
 }
 </script>
---
+++ 7/src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div>
+    <p>{{count}}</p>
+    <p>
+      <button type="button" name="button" @click="increment">+</button>
+      <button type="button" name="button" @click="decrement">-</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed : {
+    count : function() {
+      return this.$store.state.count;
+    }
+  },
+  methods: {
+    increment : function() {
+      this.$store.commit('increment');
+    },
+    decrement : function() {
+      this.$store.commit('decrement');
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
