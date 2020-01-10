8877918c4c95e00f3b36517ae09daf326df15de1
psk <psk3060@naver.com>
2020-01-10 19:12:17 +0900

    카트에 있는 물품 Print

--- 7/src/components/ShoppingCart.vue
+++ 7/src/components/ShoppingCart.vue
@@ -1,6 +1,14 @@
 <template lang="html">
   <div class="cart">
-
+    <h2>카트</h2>
+    <p v-show="!products.length"><i>카트가 비어있습니다.</i></p>
+    <ul>
+      <li
+        v-for="product in products"
+        :key="product.id">
+        {{ product.title }} - {{ product.price | currency }} x {{ product.quantity }}
+      </li>
+    </ul>
     <p>Total: {{ total | currency }}</p>
   </div>
 </template>
@@ -13,6 +21,7 @@ export default {
     ...mapState({
       checkoutStatus : state => state.cart.checkoutStatus
     }),
+    // 불러오기 위함.
     ...mapGetters('cart', {
       products : 'cartProducts',
       total : 'cartTotalPrice'
